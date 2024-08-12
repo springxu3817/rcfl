7f96b2d649a49b7fc63ab824aca936996a35f0dc
springxu <14694422+springxu3817@user.noreply.gitee.com>
2024-08-12 03:06:51 -0700

    feature: add to support directory for a static http file server.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use anyhow::Result;
 use axum::extract::State;
-use axum::response::IntoResponse;
+use axum::response::{Html, IntoResponse};
 use axum::{http::StatusCode, routing::get, Router};
 use axum_macros::debug_handler;
 use clap::{Parser, Subcommand};
+use std::ffi::OsString;
+use std::fmt::Debug;
 use std::net::SocketAddr;
 use std::{
+    io::Error,
     path::{Path, PathBuf},
     sync::Arc,
 };
 use tracing::info;
+use urlencoding::encode;
 
 #[derive(Parser, Debug)]
 #[command(name="rcli", version, author, about, long_about= None)]
@@ -47,7 +51,7 @@ struct HttpServeState {
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let opts = Opts::parse();
-    println!("test {:?}", opts);
+    println!("test change {:?}", opts);
     match opts.cmd {
         SubCommand::Http(s) => match s {
             HttpSubCommand::Serve(opts) => {
@@ -81,24 +85,82 @@ async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     Ok(())
 }
 
+// Traverse directorie
+async fn get_cur_dir_files(dir: &PathBuf) -> Result<Vec<OsString>, std::io::Error> {
+    let mut files = vec![];
+    files.clear();
+    if dir.is_dir() {
+        let mut entries = tokio::fs::read_dir(dir).await?;
+        while let Some(entry) = entries.next_entry().await? {
+            files.push(entry.file_name());
+            //println!("{:?} {:?}", entry.path(), entry.file_name());
+        }
+        //println!("vec  {:?}", files);
+        Ok(files)
+    } else {
+        Err(Error::from(std::io::ErrorKind::InvalidData))
+    }
+}
+
 #[debug_handler]
 async fn file_handler(
     State(state): State<Arc<HttpServeState>>,
     axum::extract::Path(req_path): axum::extract::Path<String>,
 ) -> (StatusCode, axum::response::Response) {
-    let p: PathBuf = std::path::Path::new(&state.path).join(req_path);
-    if !p.exists() {
+    let pathbuf: PathBuf = std::path::Path::new(&state.path).join(req_path.clone());
+    println!("file_handler DirPath {:?}", pathbuf);
+    if !pathbuf.exists() {
         // not found return code = 404
         (
             StatusCode::NOT_FOUND,
-            format!("File {:?} not found!", p).into_response(),
+            format!("File {} not found!", req_path).into_response(),
         )
+    } else if pathbuf.is_dir() {
+        let mut str_list: String = String::new();
+        let res = get_cur_dir_files(&pathbuf).await;
+        match res {
+            Err(e) => {
+                return (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    e.to_string().into_response(),
+                );
+            }
+            Ok(content) => {
+                str_list
+                    .push_str(format!("<li><a href={}>{}</li> ", encode(&req_path), ".").as_str());
+                for filename in content {
+                    let str_file_name = filename.into_string();
+                    match str_file_name {
+                        Ok(res_file_name) => {
+                            let filepath = format!("{}/{}", req_path, res_file_name);
+                            str_list.push_str(
+                                format!(
+                                    "<li><a href={}>{:?}</li> ",
+                                    encode(&filepath),
+                                    res_file_name
+                                )
+                                .as_str(),
+                            );
+                        }
+                        Err(str_os_file_name) => {
+                            str_list.push_str(format!("<li>{:?}</li> ", str_os_file_name).as_str());
+                        }
+                    }
+                }
+            }
+        }
+        let mut str_body = String::new();
+        str_body.push_str(format!("<html><body>{}</body></html>", str_list).as_str());
+        return (
+            StatusCode::OK,
+            Html::<String>::from(str_body).into_response(),
+        );
     } else {
-        match tokio::fs::read_to_string(&p).await {
+        match tokio::fs::read_to_string(&pathbuf).await {
             //read utf8 txt
             Ok(content) => (StatusCode::OK, content.into_response()), //OK 200
             Err(e) => match e.kind() {
-                std::io::ErrorKind::InvalidData => match tokio::fs::read(&p).await {
+                std::io::ErrorKind::InvalidData => match tokio::fs::read(&pathbuf).await {
                     //read octet-stream
                     Ok(byte_array) => (StatusCode::OK, byte_array.into_response()),
                     //read octet-stream error
